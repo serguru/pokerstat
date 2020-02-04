a125a59d7e1c2cb4f42f284152698b3777d1affa
Sergey Chernyshov <sergey.chernyshov@bcpharmacists.org>
2020-02-04 10:56:34 -0800

    Some progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Hand } from './helpers/hand';
-import { handValues, rawHandRankings } from './helpers/consts';
+import { rawHandRankings } from './helpers/consts';
 import { HandValue } from './helpers/hand-value';
 import { Row } from './helpers/row';
-import { playedHands } from './helpers/data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-
-  ns = [];
-  groupTotal: number;
-  normalTotal = 1326;
-  values = [];
-
+export class AppComponent {
 
   constructor() {
-    if (playedHands.length == 0) {
-      throw new Error('No played hands');
-    }
     if (this.rawHandRanking.length != this.rawLength) {
       throw new Error('Wrong raw collection length');
     }
@@ -32,16 +22,67 @@ export class AppComponent implements OnInit {
     this.updateRows();
   }
 
+  @ViewChild('file', { static: false }) file;
   rawLength = 169;
   rows: Row[] = [];
   rawHandRanking: string[] = rawHandRankings[0].value.split(','); // 169 entries
+  fileContent: any;
+  playedHands: string[];
+
+  validate(s: string): boolean {
+    let ok: boolean = /^[tjqkaTJQKA2-9]{1}[scdhSCDH]{1}[tjqkaTJQKA2-9]{1}[scdhSCDH]{1}/.test(s);
+    if (!ok) {
+      return false;
+    }
+
+    if (s.substr(0, 1).toLowerCase() != s.substr(2, 1).toLowerCase()) {
+      return true;
+    }
+
+    return s.substr(2, 1).toLowerCase() != s.substr(4, 1).toLowerCase();
+  }
+
+
+  processFile(): void {
+    if (!this.fileContent) {
+      throw new Error('No input file');
+    }
+
+    let lines: string[];
+    if (!this.fileContent.text) {
+      throw new Error('Wrong hands history file format');
+    }
+    this.fileContent.text().then((s: string) => {
+      try {
+        lines = s.split('\n').map(x => !x ? x : x.trim());
+      }
+      catch
+      {
+        throw new Error('Cannot read hands history');
+      }
+
+      if (lines.length == 0) {
+        throw new Error('No hands in this file');
+      }
+
+      for (let i = 0; i < lines.length; i++) {
+        const hand: string = lines[i];
+        if (!this.validate(hand)) {
+          throw new Error(`A hand #${i + 1} "${hand}" is invalid`)
+        }
+      }
+
+      this.playedHands = lines;
+      this.updateRows();
+    })
+  }
 
   getRawHands(start: number, length: number): string[] {
-      let result = [];
-      for (let i = start; i < start + length; i++) {
-        result.push(this.rawHandRanking[i]);
-      }
-      return result;
+    let result = [];
+    for (let i = start; i < start + length; i++) {
+      result.push(this.rawHandRanking[i]);
+    }
+    return result;
   }
 
   updateRows(): void {
@@ -53,37 +94,35 @@ export class AppComponent implements OnInit {
       let length: number = i < 9 ? 17 : 16;
       let start = i * 17;
       row.hands = this.getRawHands(start, length);
-      row.expected = length / this.rawLength * playedHands.length;
+      row.expected = length / this.rawLength * (this.playedHands ? this.playedHands.length : 0);
       row.fact = 0;
       result.push(row);
       p = p == 20 ? -20 : p - 20;
     }
 
-    playedHands.forEach(x => {
-      const raw: string = this.hand2raw(x, false);
-      const rawR: string = this.hand2raw(x, true);
-      let index = -1;
-      let row: Row;
-      for (let i = 0; i < 10; i++) {
-        row = result[i];
-        if (row.hands.find(x => x == raw || x == rawR)) {
-          index = i;
-          break;
+    if (this.playedHands) {
+      this.playedHands.forEach(x => {
+        const raw: string = this.hand2raw(x, false);
+        const rawR: string = this.hand2raw(x, true);
+        let index = -1;
+        let row: Row;
+        for (let i = 0; i < 10; i++) {
+          row = result[i];
+          if (row.hands.find(x => x == raw || x == rawR)) {
+            index = i;
+            break;
+          }
         }
-      }
 
-      if (index == -1) {
-        throw new Error(`Played hand ${raw} not found`)
-      }
+        if (index == -1) {
+          throw new Error(`Played hand ${raw} not found`)
+        }
 
-      row.fact++;
-    })
+        row.fact++;
+      })
+    }
 
     this.rows = result;
-  }
-  
-  ngOnInit(): void {
-
   }
 
   hand2raw(hand: string, reverted: boolean): string {
@@ -91,14 +130,14 @@ export class AppComponent implements OnInit {
     const fi: number = reverted ? 2 : 0;
     const si: number = reverted ? 0 : 2;
 
-    const f: string = hand.substr(fi,1).toUpperCase();
-    const s: string = hand.substr(si,1).toUpperCase();
+    const f: string = hand.substr(fi, 1).toUpperCase();
+    const s: string = hand.substr(si, 1).toUpperCase();
     let result: string = f + s;
     if (f == s) {
       return result;
     }
-    const suited: boolean = hand.substr(1,1).toUpperCase() == hand.substr(3,1).toUpperCase();
-    result +=  suited ? 's' : 'o';
+    const suited: boolean = hand.substr(1, 1).toUpperCase() == hand.substr(3, 1).toUpperCase();
+    result += suited ? 's' : 'o';
     return result;
   }
 
@@ -108,72 +147,67 @@ export class AppComponent implements OnInit {
       v += x.fact * x.success
     })
 
-    v = v / playedHands.length;
+    if (!this.playedHands) {
+      return "";
+    }
+
+    v = v / this.playedHands.length;
 
     return (v < 0 ? 'Failure ' : 'Success ') + v.toFixed(2) + '%';
   }
 
   get handsTotal(): string {
-    return playedHands.length.toString();
+    return this.playedHands ? this.playedHands.length.toString() : "no hands";
   }
 
-  // ngOnInit(): void {
 
-  //   const result: GroupValue[] = [];
-  //   this.groupTotal = 0;
+  onFileOpen() {
+    if (!this.file.nativeElement.files || this.file.nativeElement.files.length == 0) {
+      return;
+    }
+    this.fileContent = this.file.nativeElement.files[0];
 
-  //   for (let i = 1; i <= 9; i++) {
-  //     const a = [];
-  //     hands.forEach(x => a.push(0));
-  //     const value = new GroupValue(i, 0, 0);
-  //     result.push(value);
-  //   }
+    this.processFile();
+  }
 
-  //   handValues.forEach(x => {
-  //     if (x.id.substr(0, 1) == x.id.substr(1, 1)) {
-  //       result[x.notSuited - 1].normal = result[x.notSuited - 1].normal + 6;
-  //       this.groupTotal = this.groupTotal + 6;
-  //     } else {
-  //       result[x.suited - 1].normal = result[x.suited - 1].normal + 4;
-  //       result[x.notSuited - 1].normal = result[x.notSuited - 1].normal + 12;
-  //       this.groupTotal = this.groupTotal + 16;
-  //     }
-  //   })
+  selectFile() {
+    this.file.nativeElement.click();
+  }
 
-  //   if (this.groupTotal !== this.normalTotal) {
-  //     throw new Error(`Total != ${this.normalTotal} !!!`)
-  //   }
+  get fileName(): string {
+    return this.fileContent ? this.fileContent.name : 'hands';
+  }
 
-  //   const ns = [];
-
-  //   hands.forEach(x => {
-  //     const hand = Hand.str2hand(x);
-  //     ns.push(hand);
-  //     const handValue: number = this.getHandValue(hand);
-  //     result[handValue - 1].fact++;
-  //   })
-
-  //   result.forEach(x => {
-  //     x.normal = Math.round(x.normal / this.normalTotal * 100);
-  //     x.fact = Math.round(x.fact / hands.length * 100);
-  //   })
-
-  //   this.ns = ns;
-  //   this.values = result;
-  // }
-
-  getHandValue(hand: Hand): number {
-    let handValue: HandValue = handValues.find(x => {
-      const result: boolean = x.id == hand.caption || x.id == hand.captionReverted;
-      return result;
-    });
-
-    if (!handValue) {
-      throw new Error('Hand value not found')
+  saveFile() {
+    if (!this.playedHands) {
+      return;
     }
 
-    return hand.isSuited ? handValue.suited : handValue.notSuited;
+    let s: string = "";
+
+    this.playedHands.forEach(x => {
+      s += x + '\n'
+    })
+
+    var file = new Blob([s], { type: "text/plain" });
+
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+      window.navigator.msSaveOrOpenBlob(file, this.fileName);
+    else { // Others
+      const a = document.createElement("a"),
+        url = URL.createObjectURL(file);
+      a.href = url;
+      a.download = this.fileName;
+      document.body.appendChild(a);
+      a.click();
+      setTimeout(function () {
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+      }, 0);
+    }
+
   }
+
 }
 
 
